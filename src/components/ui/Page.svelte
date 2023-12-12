4cccff7bc88271beb9a6497717ebc2ee2141a92f
Rémi Bétin <git@remibetin.fr>
2023-12-12 15:16:29 +0100

    t# This is a combination of 2 commits.
    Add "draft" banner for translations

--- src/components/ui/Page.svelte
+++ src/components/ui/Page.svelte
@@ -7,6 +7,13 @@
   <div class="default-grid">
     <main class="main-with-panel">
       <h1>{title}</h1>
+      {#if currentLocale.status === "draft"}
+          <section id="translation-info" class="doc-note-translation" lang="en">
+            <h2 class="visuallyhidden">About this translation</h2>
+            <p>This volunteer translation is a <strong>draft</strong>. It contains English wording that will be translated soon.</p>
+            <p>See <a href="{`${$basepath}/acknowledgements`}">Acknowledgements</a> for all translators and contributors.</p>
+          </section>
+      {/if}
       <slot />
     </main>
     <YourReport />
@@ -18,6 +25,13 @@
       <YourReport />
       {/if}
       <h1>{title}</h1>
+      {#if currentLocale.status === "draft"}
+          <section id="translation-info" class="doc-note-translation" lang="en">
+            <h2 class="visuallyhidden">About this translation</h2>
+            <p>This volunteer translation is a <strong>draft</strong>. It contains English wording that will be translated soon.</p>
+            <p>See <a href="{`${$basepath}/acknowledgements`}">Acknowledgements</a> for all translators and contributors.</p>
+          </section>
+      {/if}
       <slot />
     </main>
   </div>
@@ -43,8 +57,12 @@
   import { onMount } from "svelte";
   import { useLocation } from 'svelte-navigator';
   import { honourFragmentIdLinks } from '@app/scripts/honourFragmentIdLinks.js';
+  import { locale } from 'svelte-i18n';
 
-  import { routes, yourReportPanelOpen } from '@app/stores/appStore.js';
+  import locales from '@app/locales/index.json';
+  $: currentLocale = locales.find((l) => l.lang === $locale);
+
+  import { routes, basepath, yourReportPanelOpen } from '@app/stores/appStore.js';
 
   import YourReport from '@app/components/ui/YourReport.svelte';
 
